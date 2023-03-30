266b190162a2c44d3e74ecabf426a4dd5e841b58
jsirianni <joe.sirianni@observiq.com>
2023-03-30 16:23:34 -0400

    try different work dir

--- test/cases/default/inspec.rb
+++ test/cases/default/inspec.rb
@@ -1,17 +1,11 @@
-control "k8s-app-validate" do
-  impact 1.0
-  title "Validate K8s test Application"
-  desc "The k8s-app test app should exist and be running"
-  
-  k8sobjects(api: 'v1', type: 'pods', namespace: 'default', labelSelector: 'app.kubernetes.io/name=bindplane').items.each do |pod|
+k8sobjects(api: 'v1', type: 'pods', namespace: 'default', labelSelector: 'app.kubernetes.io/name=bindplane').items.each do |pod|
     describe "#{pod.namespace}/#{pod.name} pod" do
-      subject { k8sobject(api: 'v1', type: 'pods', namespace: pod.namespace, name: pod.name) }
-      it { should exist }
-      it { should be_running }
+        subject { k8sobject(api: 'v1', type: 'pods', namespace: pod.namespace, name: pod.name) }
+        it { should exist }
+        it { should be_running }
     end
-  end
+end
 
-  describe k8sobjects(api: 'v1', type: 'services', namespace: 'default', labelSelector: 'app.kubernetes.io/name=bindplane') do
+describe k8sobjects(api: 'v1', type: 'services', namespace: 'default', labelSelector: 'app.kubernetes.io/name=bindplane') do
     it { should exist }
-  end
 end

d6e79959727c98fd6b1534742b5445a6bc52707c
Austin Georgiades <algchoo@gmail.com>
2024-09-12 15:23:35 -0400

    fix path to readme

--- .github/workflows/scripts/check-helm-docs.sh
+++ .github/workflows/scripts/check-helm-docs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! git diff --exit-code --quiet '../../charts/bindplane/README.md'; then
+if ! git diff --exit-code --quiet 'charts/bindplane/README.md'; then
     echo "ERROR: Uncommitted helm doc changes detected. Please commit your changes before running this script."
     exit 1
 fi

c0101eed8401103b3c1e6e6cc9bf5512ae8598e3
Austin Georgiades <34201358+algchoo@users.noreply.github.com>
2024-09-13 09:14:59 -0400

    feat: Update CI to run `helm-docs` and fail if changes exist (#160)
    * add script to check diff for chart README
    
    * add steps to helm job to run helm-docs and check the diff
    
    * make script executable
    
    * fix path to readme
    
    * fix path to readme
    
    * fix path to readme
    
    * fix path to readme
    
    * ran helm-docs

---
+++ .github/workflows/scripts/check-helm-docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+if ! git diff --exit-code --quiet 'charts/bindplane/README.md'; then
+    echo "ERROR: Uncommitted helm doc changes detected. Please commit your changes before running this script."
+    exit 1
+fi

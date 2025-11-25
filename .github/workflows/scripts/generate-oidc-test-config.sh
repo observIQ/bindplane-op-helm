de5b01f3a44852147d2be185b1bd84606f851808
Joseph Sirianni <joe.sirianni@observiq.com>
2025-11-25 16:24:32 -0500

    feat(oauth): Support reading secret from k8s secret (#220)
    * feat(oauth): Support reading secret from k8s secret
    
    * tweak new option name

--- .github/workflows/scripts/generate-oidc-test-config.sh
+++ .github/workflows/scripts/generate-oidc-test-config.sh
@@ -50,7 +50,9 @@ auth:
   oidc:
     issuer: "https://accounts.google.com"
     oauth2ClientID: "$client_id"
-    oauth2ClientSecret: "$client_secret"
+    oauth2ClientSecretRef:
+      name: bindplane-oidc-secret
+      key: oauth2ClientSecret
     scopes: [
       "openid",
       "profile",
